b1817b741c28cc887e231a5663ba80c09aca44ec
agustinelson <an.medinacolmenero@gmail.com>
2023-02-24 13:31:14 -0600

    update plots.py

--- proto_franck-hertz/pythonfiles/plots.py
+++ proto_franck-hertz/pythonfiles/plots.py
@@ -12,25 +12,42 @@ import os
 
 namefiles = sorted(os.listdir('data'))
 
-inf = 17
-
+inf = 0
+nplots = 5
 
 i = 0
 ax = []
 
-#plt.figure(figsize=(6,6))
 
-for namefile in namefiles[inf:inf+6]:
+
+for namefile in namefiles[inf:inf+nplots]:
     print(namefile)
     data = np.genfromtxt(f'data/{namefile}', usecols=(0,-2,-1), comments="#")
-    ax.append(plt.subplot(6,1,1+i))
-    plt.errorbar(data[:,0], data[:,1], yerr=data[:,2], xerr=0.005, label=(f"{i+1}.00V"), c = "k")
+    ax.append(plt.subplot(nplots,1,1+i))
+    
+    plt.errorbar(data[:,0], data[:,1],
+                 yerr=data[:,2], xerr=0.005, 
+                 label=(f"{i+1}.00V"), 
+                 c="k",
+                 marker =".",barsabove="True",
+                 ecolor="k")
     
     i+=1
     
     
 plt.xlabel(r"Acceleration Voltage ($V$)")
-ax[3].set_ylabel(r"Electrical current ($\mu A$)")
-plt.savefig(f"plots/collectorvoltage_{26}.00V_{31}.00V.svg")
+ax[int(nplots/2)].set_ylabel(r"Electric current ($\mu A$)")
+
+
+for _ax in ax: _ax.set_ylim((0.01,1))
+
+
+#ax[3].set_ylim((0.10,0.15))
+
+#ax[3].set_ylabel(r"Electrical current ($\mu A$)")
+
+
+#plt.show()
+plt.savefig(f"plots/collectorvoltage_{inf+1}.00V_{inf+nplots}.00V.svg")
 
     
