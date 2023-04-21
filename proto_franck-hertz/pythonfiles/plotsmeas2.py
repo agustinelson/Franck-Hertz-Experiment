d67ac39fd5e3d39df5c7bdecfcef5186e9ed7514
agustinelson <an.medinacolmenero@gmail.com>
2023-04-21 15:50:47 -0600

    Add files to plot using python

---
+++ proto_franck-hertz/pythonfiles/plotsmeas2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 15:42:02 2023
+
+@author: legacy
+"""
+
+import pandas as pd
+
+print(pd.read_csv("measurements2/collectorVoltaje1.00V/descelerationVoltaje1.00V/meas0001.csv"))
