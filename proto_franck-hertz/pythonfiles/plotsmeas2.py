4b75c884cd97fb1b1ed80616eafd9662843cc069
agustinelson <an.medinacolmenero@gmail.com>
2023-04-21 16:48:39 -0600

    Update plotsmeas2.py

--- proto_franck-hertz/pythonfiles/plotsmeas2.py
+++ proto_franck-hertz/pythonfiles/plotsmeas2.py
@@ -5,7 +5,24 @@ Created on Fri Apr 21 15:42:02 2023
 
 @author: legacy
 """
-
+#%%%
 import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+#%%%
+files = os.listdir(os.path.dirname(os.path.abspath(__file__))+"/measurements2/collectorVoltage3.00V/decelerationVoltage1.00V")
+files.remove('README.md')
+files.sort()
+print(files)
+#%%%
 
-print(pd.read_csv("measurements2/collectorVoltaje1.00V/descelerationVoltaje1.00V/meas0001.csv"))
+voltage=np.arange(32)
+#%%%
+
+data = []
+for file in files:
+    file=path=os.path.dirname(os.path.abspath(__file__))+"/measurements2/collectorVoltage3.00V/decelerationVoltage1.00V/"+file
+    data.append(pd.read_csv(file)["Unnamed: 1"].dropna().mean())
+#%%%
+plt.plot(voltage, data)
